6d64d79d5d58afeaef906fdb7afdd0ceb109ca5c
tang_yue1129@163.com <tangyue5@tal.com>
2021-08-18 19:36:05 +0800

    u

---
+++ coding/leetCode/6.zig-zag-conversion.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=6 lang=javascript
+ *
+ * [6] ZigZag Conversion
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+  // 大概明白意思
+  // 字符串以几行的z字形排布，那请问你有什么思路吗？
+  // 0 6 12
+  // 1 5 7 11 13
+  // 2 4 8 10 
+  // 3 9
+  // 这个
+  // 暂时搁置
+};
+// @lc code=end
+
