f8222af9800e3b61c15314bbc12c5cd854082af4
tang_yue1129@163.com <tangyue5@tal.com>
2021-09-10 18:19:27 +0800

    test

--- coding/leetCode/10.regular-expression-matching.js
+++ coding/leetCode/10.regular-expression-matching.js
@@ -10,19 +10,19 @@
  * @param {string} p
  * @return {boolean}
  */
-var isMatch = function(s, p) {
+var isMatch = function (s, p) {
   if (p.length > s.length) return false;
   // 遍历p字符串
   for (let i = 0; i < p.length; i++) {
 
   }
-  
+
 };
 // @lc code=end
 
 /*
 explain: 题目就是考察正则
 这个我有什么思路呢，如果对的话，那么就补齐，跳过
-居然没有思路了
+居然没有思路了, 测试
 */
 
