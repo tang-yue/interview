fb1ae2d65c4dc36553a0a56ae97aa661b5881b5a
tangyue <tang_yue@foxmail.com>
2021-10-12 21:50:07 +0800

    u

--- coding/leetCode/91.decode-ways.js
+++ coding/leetCode/91.decode-ways.js
@@ -9,16 +9,35 @@
  * @param {string} s
  * @return {number}
  */
-var numDecodings = function(s) {
-  for (var v of s) {
-    
-  }
+ var numDecodings = function(s) {
+  // 递归解法
+  
 };
 // @lc code=end
 
 /*
 描述：按照规定的编码方式，返回的字母组合方式有多少种。
 题解：
-可能是一位拆分，也可能是两位拆分
+暴力回溯法，显示超时
+var numDecodings = function(s) {
+  let count = 0;
+  var dfs = (str, start) => {
+    if(str.length === s.length) {
+      count++;
+      return;
+    }
+    if(Number(s[start]) !== 0) {
+      if(Number(s[start] + s[start+1]) <= 26) {
+        dfs(str+s[start] + s[start+1], start+2);
+      }
+    }
+    if(Number(s[start]) !== 0) {
+      dfs(str+s[start], start+1);
+    }
+    if(Number(s[start]) === 0) return; 
+  }
+  dfs('', 0)
+  return count;
+};
 */
 
