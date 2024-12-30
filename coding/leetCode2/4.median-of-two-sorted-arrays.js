87cceb4d3074597b11284083d697bc6ba558c679
tang-yue <tang_yue@foxmail.com>
2024-12-30 20:52:35 +0800

    feat: udpate

---
+++ coding/leetCode2/4.median-of-two-sorted-arrays.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=4 lang=javascript
+ *
+ * [4] Median of Two Sorted Arrays
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    // nums1 和 nums2 都是有序的
+    let arr = []
+    for(let i = 0; i < nums2.length; i++) {
+        // nums1.push(nums2[i]);
+        
+    }
+};
+// @lc code=end
+
