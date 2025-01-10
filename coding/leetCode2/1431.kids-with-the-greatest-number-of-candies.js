67590237732c0d8a8eb0cb93775fa6b25d812211
tang-yue <tang_yue@foxmail.com>
2025-01-10 11:33:00 +0800

    feat: add questions

---
+++ coding/leetCode2/1431.kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1431 lang=javascript
+ *
+ * [1431] Kids With the Greatest Number of Candies
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    
+};
+// @lc code=end
+
