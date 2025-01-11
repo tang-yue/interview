26d41608becd7a27fc5435cbe83f8ccc40d9ea9d
tang-yue <tang_yue@foxmail.com>
2025-01-11 17:51:37 +0800

    feat: add 345 605

--- coding/leetCode2/1431.kids-with-the-greatest-number-of-candies.js
+++ coding/leetCode2/1431.kids-with-the-greatest-number-of-candies.js
@@ -11,7 +11,18 @@
  * @return {boolean[]}
  */
 var kidsWithCandies = function(candies, extraCandies) {
-    
+  // 初始化candies 数组长度的新数组，内容为false
+  let result = new Array(candies.length).fill(false);
+  // 遍历candies
+  for(let i = 0; i < candies.length; i++) {
+    // 判断是否有孩子拥有最多的糖果
+    if(candies[i] + extraCandies >= Math.max(...candies)) {
+      result[i] = true;
+    }
+  }
+  return result;
 };
 // @lc code=end
+
+// 这道题相对还是比较简单的
 
---
+++ coding/leetCode2/345.reverse-vowels-of-a-string.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=345 lang=javascript
+ *
+ * [345] Reverse Vowels of a String
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+    
+};
+// @lc code=end
+
