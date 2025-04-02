5bfaa7e5fc3376b4cca6e43c1675a15bbe5a8fdf
tang-yue <tang_yue@foxmail.com>
2025-04-02 11:24:50 +0800

    feat: add 643

--- coding/leetCode2/643.maximum-average-subarray-i.js
+++ coding/leetCode2/643.maximum-average-subarray-i.js
@@ -12,11 +12,34 @@
  */
 var findMaxAverage = function(nums, k) {
     let maxAverage = -Infinity
-    if(nums.length <= k) return nums.reduce((a, b) => a + b, 0) / nums.length;
-    for(let i = 0; i < nums.length - k + 1; i++) {
-      maxAverage = Math.max(maxAverage, (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3]) / k)
+    let sum = 0
+    for(let i = 0; i < k; i++) {
+        sum += nums[i]
+    }
+    maxAverage = Math.max(maxAverage, sum / k)
+    for(let i = k; i < nums.length; i++) {
+        sum += nums[i] - nums[i - k]
+        maxAverage = Math.max(maxAverage, sum / k)
     }
     return maxAverage
 };
 // @lc code=end
+// 滑动窗口解法，时间复杂度为O(n) 真是太妙了赞赞赞！！
+// 除了如下的解法，我并没有想到其他的方法
+/*
+// 暴力解法 但是超时了，时间复杂度为O(n^2)
+var findMaxAverage = function(nums, k) {
+    let maxAverage = -Infinity
+    if(nums.length <= k) return nums.reduce((a, b) => a + b, 0) / nums.length;
+    for(let i = 0; i < nums.length - k + 1; i++) {
+      let sum = 0
+      for(let j = 0; j < k; j++) {
+        sum += nums[i + j]
+      }
+      maxAverage = Math.max(maxAverage, sum / k)
+    }
+    return maxAverage
+};
+*/
+
 
