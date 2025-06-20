ce30e4ff9352c1cff8376aff715a917599cee329
tang-yue <tang_yue@foxmail.com>
2025-06-20 18:56:09 +0800

    feat: add 2130

--- coding/leetCode2/206.reverse-linked-list.js
+++ coding/leetCode2/206.reverse-linked-list.js
@@ -17,9 +17,23 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    
+  let current = head;
+  let prev = null
+  // 遍历链表
+  while (current !== null) {
+    let next = current.next
+    current.next = prev
+    prev = current
+    current = next
+  }
+  return prev
 };
 // @lc code=end
 
-// 
+// 这道题题目意思比较好理解
+// 可以倒遍历链表吗， 不可以
+// 本质上是把指针的指向发生改变，从指向右，到变成指向左，
+// 而我们的惯性思维是先取出最后一个，然后再取出倒数第二个，然后顺序拼接
+// AI 给了思路， 自己默写一遍吧
+
 
---
+++ coding/leetCode2/2130.maximum-twin-sum-of-a-linked-list.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=2130 lang=javascript
+ *
+ * [2130] Maximum Twin Sum of a Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var pairSum = function(head) {
+    
+};
+// @lc code=end
+// 完全没有思路
